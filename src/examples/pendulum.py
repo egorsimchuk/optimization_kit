a64596293baef467862d5a63a46047568380074b
esimchuk <e.simchuk@conundrum.ai>
2022-10-30 16:40:01 +0300

    Setup experiment with hydra configuration

--- src/examples/pendulum.py
+++ src/examples/pendulum.py
@@ -11,18 +11,27 @@ from loguru import logger
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
-
+import hydra
+from omegaconf import DictConfig, OmegaConf
 from actors.q_learning_actor import QMatrix, QLearningActor
 from gym.envs.classic_control.pendulum import angle_normalize
 
 from utils.plot import save_multiple_formats
 
-MAX_EPISODE_STEPS = 300
-DATA_FOLDER = Path(__file__).parents[2] / "data/pendulum"
+DATA_FOLDER = Path(__file__).parents[2] / "data"
 
 
-def train(filepath, q_matrix, actor):
-    env = gym.make('Pendulum-v1', g=9.81, max_episode_steps=MAX_EPISODE_STEPS)
+def init_env(config, render_mode=None):
+    env_conf = dict(config.environment)
+    max_torque = env_conf.pop("max_torque")
+    env = gym.make(render_mode=render_mode, **env_conf)
+    env.env.env.env.max_torque = max_torque
+    return env
+
+
+def train(config: DictConfig, q_matrix, actor):
+    env = init_env(config)
+    filepath = DATA_FOLDER / config.weights_fpath
     if filepath.exists():
         logger.info(f"Start. Use matrix from {filepath}")
         q_matrix.load(filepath)
@@ -30,15 +39,16 @@ def train(filepath, q_matrix, actor):
         logger.info("Start. Use all zero matrix")
 
     info = []
-    for i in tqdm.tqdm(range(1000)):
-        run_simulation(env, actor, n_step=10000)
+    for i in tqdm.tqdm(range(config.train_params.n_batches)):
+        run_simulation(env, actor, n_step=config.train_params.n_step)
         info.append({"iter": i, "zero_percentage": np.sum(q_matrix.matrix == 0) / np.prod(q_matrix.matrix.shape)})
-        if i % 100 == 0:
+        if i % config.train_params.batch_plot == 0:
             visualize_matrix(q_matrix, title=f"State space exploration, iter {i}")
 
     env.close()
     visualize_info(info)
     q_matrix.save(filepath)
+    logger.info(f"Matrix updated: {filepath}")
 
 
 def visualize_matrix(q_matrix, title):
@@ -48,7 +58,7 @@ def visualize_matrix(q_matrix, title):
         y=q_matrix.state_space[0],
         colorscale='Viridis'))
     fig.update_layout(title=title, xaxis_title="Angular velocity", yaxis_title="Angle")
-    save_multiple_formats(fig, DATA_FOLDER / f"matrix_max_updates/{title}")
+    save_multiple_formats(fig, DATA_FOLDER / f"pendulum/matrix_max_updates/{title}")
 
     for angle_idx in [5, 15]:
         plot_matrix = q_matrix.matrix[angle_idx]
@@ -58,12 +68,12 @@ def visualize_matrix(q_matrix, title):
             y=q_matrix.state_space[1],
             colorscale='Viridis'))
         fig.update_layout(title=f"{title}. Angle {q_matrix.state_space[0][angle_idx]}", yaxis_title="Angular velocity", xaxis_title="Momentum")
-        save_multiple_formats(fig, DATA_FOLDER / f"angular_idx{angle_idx}_updates/{title}")
+        save_multiple_formats(fig, DATA_FOLDER / f"pendulum/angular_idx{angle_idx}_updates/{title}")
 
 
 def visualize_info(info):
     fig = px.line(pd.DataFrame(info), x="iter", y="zero_percentage")
-    save_multiple_formats(fig, DATA_FOLDER / f"zero_percentage_progress")
+    save_multiple_formats(fig, DATA_FOLDER / f"pendulum/zero_percentage_progress")
 
 
 def run_simulation(env, actor, n_step, sleep_seconds: float = 0):
@@ -80,33 +90,31 @@ def run_simulation(env, actor, n_step, sleep_seconds: float = 0):
             sleep(sleep_seconds)
 
 
-def test(filepath, q_matrix, actor):
-    env = gym.make('Pendulum-v1', g=9.81, render_mode="human", max_episode_steps=MAX_EPISODE_STEPS)
+def test(config, q_matrix, actor):
+    filepath = DATA_FOLDER / config.weights_fpath
+    env = init_env(config, render_mode="human")
     q_matrix.load(filepath)
-    q_matrix.matrix
-    run_simulation(env, actor, n_step=1000, sleep_seconds=0.1)
+    run_simulation(env, actor, n_step=config.test_params.n_step, sleep_seconds=config.test_params.sleep_seconds)
     env.close()
 
 
-if __name__ == '__main__':
+@hydra.main(version_base=None, config_path="../../config/pendulum", config_name="q_learning")
+def main(config: DictConfig) -> None:
+    logger.info(f"Configuration:\n{OmegaConf.to_yaml(config)}")
+
     logger.remove()
-    logger.add(sys.stderr, level="INFO")
+    logger.add(sys.stderr, level=config.logger_level)
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--mode')
-    args = parser.parse_args()
+    q_matrix = QMatrix(**config.q_matrix)
+    actor = QLearningActor(q_matrix=q_matrix, **config.actor)
 
-    learning_rate = 1
-    discount_factor = 1
-    explore_rate = 0.5
-    q_matrix = QMatrix(state_bounds=[(-np.pi / 2, np.pi / 2), (-1, 1)], state_granularity=[0.1, 0.1],
-                       action_bounds=[(-2, 2)], action_granularity=[0.1])
-    actor = QLearningActor(learning_rate=learning_rate, discount_factor=discount_factor, q_matrix=q_matrix, explore_rate=explore_rate)
-    filepath = DATA_FOLDER / "pendulum_q_weights_exprate05.npy"
-
-    if args.mode == 'train':
-        train(filepath, q_matrix, actor)
-    elif args.mode == 'test':
-        test(filepath, q_matrix, actor)
+    if config.mode == "train":
+        train(config, q_matrix, actor)
+    elif config.mode == "test":
+        test(config, q_matrix, actor)
     else:
-        raise ValueError("--mode argument should be train or test")
+        raise ValueError("mode parameter should be train or test")
+
+
+if __name__ == '__main__':
+    main()
